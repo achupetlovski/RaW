eb73d813a4dfddb21a3a54155f5921dc65fe44b5
achupetlovski <achupetlovski@gmail.com>
2017-11-23 18:21:36 +0200

    First commit, open file added

---
+++ mainwindow.cpp
@@ -0,0 +1,77 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QFile>
+#include <QDebug>
+#include <QString>
+#include <QPlainTextEdit>
+#include <QTextEdit>
+#include <QTextStream>
+
+//This function gets the contents of a file and reads it all/puts it into a file stream
+void MainWindow::read(QString Filename)
+{
+    //Creating new QFile object
+    QFile mFile(Filename);
+
+    //Check if file opens
+    if(!mFile.open(QFile::ReadOnly | QFile::Text))
+    {
+            qDebug() << "Could not open file for reading.";
+            return;
+    }
+
+    //If file does open, get contents to a stream
+    QTextStream in(&mFile);
+
+    //Get text stream contents to a QString
+    QString mFileStream = in.readAll();
+
+    //Debug in the QTconsole if you want to test
+    //qDebug() << mFileStream;
+
+    //Set the text in the editor to the QString above
+    ui->mTextEdit->setPlainText(mFileStream);
+
+    //Flush is not needed really as we don't write to the file
+    mFile.flush();
+
+    //Closing the file after we're done naturally
+    mFile.close();
+}
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    //Setting the mTextEdit object as the main one, i.e. take all the space available
+    setCentralWidget(ui->mTextEdit);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_actionOpen_triggered()
+{
+    QString fileName;
+
+    // Here we get the path/filename in the QString variable fileName
+    fileName = QFileDialog::getOpenFileName(this,
+        tr("Open file"), "/home/mike/qt/mini-projects/read-and-write-file/", tr("Lua Files (*.lua)"));
+
+    //If filename is not empty, i.e. nothing, send it to the read function for opening
+    //maybe this will need to be changed over time
+    if(fileName!="")
+    {
+        read(fileName);
+    }
+}
+
+void MainWindow::on_actionSave_triggered()
+{
+
+}
---
+++ mainwindow.h
@@ -0,0 +1,28 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+    void read(QString Filename);
+private slots:
+    void on_actionOpen_triggered();
+
+    void on_actionSave_triggered();
+
+private:
+    Ui::MainWindow *ui;
+    QString Filename;
+};
+
+#endif // MAINWINDOW_H
